56b53aba9d3cb3e01c5f0ca3bd62e3c5946d48e5
Phạm Trần Trung <trungpt@qsoft.com.vn>
2014-02-16 22:35:17 +0700

    package

---
+++ src/com/example/RecordCall_SMS/savesms/SaveSMS.java
@@ -0,0 +1,12 @@
+package com.example.RecordCall_SMS.savesms;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trung
+ * Date: 2/16/14
+ * Time: 10:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SaveSMS
+{
+}
